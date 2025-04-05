0113db8db9107e831679cdf8c05fae72d89d985e
iulia2007 <craciunescuiulia2007@gmail.com>
2025-04-05 18:05:54 +0300

    searchbar

---
+++ Folder-Diverse/js/search.js
@@ -0,0 +1,13 @@
+function searchProducts() {
+    const input = document.getElementById("searchBar").value.toLowerCase();
+    const products = document.querySelectorAll(".grid-item");
+  
+    products.forEach((product) => {
+      const productName = product.querySelector("p").textContent.toLowerCase();
+      if (productName.includes(input)) {
+        product.style.display = "block"; // Afișează produsul
+      } else {
+        product.style.display = "none"; // Ascunde produsul
+      }
+    });
+  }
